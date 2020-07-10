d8c45b8ed5f851ae0e3c3a49250c357ef80173a1
Uwem Akan Nkereuwem <uwemakan12@gmail.com>
2020-07-10 02:02:53 +0100

    Updates

---
+++ precache-manifest.157bcbc5ce734223ad054f1d771c6313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3530fba49cde7ab0e8741453dad299af",
+    "url": "/chat-client/index.html"
+  },
+  {
+    "revision": "157c1c264890afbdcbd2",
+    "url": "/chat-client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "076bb4567abad0417383",
+    "url": "/chat-client/static/js/2.e29f147c.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/chat-client/static/js/2.e29f147c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "157c1c264890afbdcbd2",
+    "url": "/chat-client/static/js/main.66fd5c2d.chunk.js"
+  },
+  {
+    "revision": "96b74d958c4089727dd5",
+    "url": "/chat-client/static/js/runtime-main.03557611.js"
+  }
+]);
